5860319f8f80e79cb7572bde19c87da48ae282e0
Fumito Ito <weathercook@gmail.com>
2025-06-13 00:39:11 +0900

    apply tests

--- Tests/OptimisticJSONParserTests/OptimisticJSONParserTests.swift
+++ Tests/OptimisticJSONParserTests/OptimisticJSONParserTests.swift
@@ -18,7 +18,22 @@ let parsedResults = [
 ]
 
 @Test(arguments: zip(brokenJSONString, parsedResults))
-func optimizeBrokenJSON(jsonString: String, expectedResult: String) async throws {
+func parseBrokenJSONString(jsonString: String, expectedResult: String) throws {
     let result = try #require(OptimisticJSONParser().parse(jsonString))
     #expect(String(describing: result) == expectedResult, "Expected \(expectedResult), got \(result)")
+}
+
+@Test
+func castParsedJSONtoDictionary() throws {
+    let result = try #require(OptimisticJSONParser().parse("{\"coordinates\":[{\"x\":1.0"))
+
+    let dictionary = try #require(result as? [String: Any])
+    #expect(dictionary.keys.count == 1, "Expected 1 key, got \(dictionary.keys.count)")
+    #expect(dictionary.keys.first == "coordinates", "Expected key 'coordinates', got \(String(describing: dictionary.keys.first))")
+
+    let value = try #require(dictionary["coordinates"] as? [[String: Any]])
+    #expect(value.count == 1, "Expected 1 item in coordinates, got \(value.count)")
+    #expect(value[0].keys.count == 1, "Expected 1 key in coordinates item, got \(value[0].keys.count)")
+    #expect(value[0].keys.first == "x", "Expected key 'x', got \(String(describing: value[0].keys.first))")
+    #expect(value[0]["x"] as? Double == 1.0, "Expected x to be 1.0, got \(String(describing: value[0]["x"]))")
 }
